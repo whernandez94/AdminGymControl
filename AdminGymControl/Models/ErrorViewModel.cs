b855201cccacaf22acfb64311f0003a26236335e
williamhdz94 <williamhernandez450@gmail.com>
2025-05-30 14:39:24 -0600

    Agregar archivos de proyecto.

---
+++ AdminGymControl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminGymControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
